c326025f998d71ccea7cbef6b04e6642bf284a4b
Maxac153 <79134041+Maxac153@users.noreply.github.com>
2022-06-03 14:52:05 +0500

    Update addLinks.js
    Delete ""

--- renderer/js/addLinks.js
+++ renderer/js/addLinks.js
@@ -5,5 +5,5 @@ document.querySelector(".create-cards-post").addEventListener("click", (e) => {
   let numberCard = [];
   links = document.querySelector(".links").value.split("\n");
   numberCard = document.querySelector(".number-card").value.split("\n");
-  ipcRenderer.send("add-cards", links, numberCard);
+  ipcRenderer.send("add-cards", links.filter(link => link !== ""), numberCard.filter(link => link !== ""));
 });
